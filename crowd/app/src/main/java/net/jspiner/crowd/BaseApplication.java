e693464908c40f76683460b8087001c115a8ce78
jspiner <jspiner@naver.com>
2018-05-12 04:22:09 +0900

    init

---
+++ crowd/app/src/main/java/net/jspiner/crowd/BaseApplication.java
@@ -0,0 +1,11 @@
+package net.jspiner.crowd;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ crowd/app/src/main/java/net/jspiner/crowd/MainActivity.java
@@ -0,0 +1,65 @@
+package net.jspiner.crowd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        init();
+    }
+
+    private void init() {
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.i("TAG", "onSuccess");
+                Log.i("TAG", "token : " + loginResult.getAccessToken().getToken());
+                Log.i("TAG", "id : " + loginResult.getAccessToken().getUserId());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("TAG", "onCancel");
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                exception.printStackTrace();
+                Log.i("TAG", "onError : " + exception.getMessage());
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
